df6821de2d89253b26313221d83398451af1f37c
Le Quang Vinh <152072416+quangvinh37@users.noreply.github.com>
2025-02-12 21:54:42 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { ChangeEvent, useRef, useState } from 'react';
+import { IRefPhaserGame, PhaserGame } from './game/PhaserGame';
+import { MainMenu } from './game/scenes/MainMenu';
+
+function App()
+{
+    //  References to the PhaserGame component (game and scene are exposed)
+    const phaserRef = useRef<IRefPhaserGame | null>(null);
+    const [insideOutMessage, setInsideOutMessage] = useState('');
+    const [outsideInMessage, setOutsideInMessage] = useState('');
+
+    const outsideIn = () => {
+
+        if(phaserRef.current)
+        {     
+            const scene = phaserRef.current.scene as MainMenu;
+            
+            if (scene)
+            {
+                scene.outsideIn(outsideInMessage, (message) => {
+                    setInsideOutMessage(message);
+                });
+            }
+        }
+    }
+
+    function onMessageChange(e: any) {
+        setOutsideInMessage(e.target.value);
+    }
+
+    // Event emitted from the PhaserGame component
+    const currentScene = (scene: Phaser.Scene) => {}
+
+    return (
+        <div id="app">
+            <PhaserGame ref={phaserRef} currentActiveScene={currentScene} />
+            <div>
+
+            </div>
+        </div>
+    )
+}
+
+export default App
---
+++ src/game/main.ts
@@ -0,0 +1,59 @@
+
+
+import { AUTO, Game } from "phaser";
+import Boot from "./scenes/Boot";
+import LoadingScene from "./scenes/LoadingScene";
+import Preloader from "./scenes/Preloader";
+import MainMenu from "./scenes/MainMenu";
+import GameOver from "./scenes/GameOver";
+import MainGame from './scenes/Game';
+
+
+
+//  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
+const config: Phaser.Types.Core.GameConfig = {
+    type: AUTO,
+    width: 1920,
+    height: 1080,
+    parent: 'phaser-example',
+    backgroundColor: '#FFFFFF',
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        max: {
+            width: 1920, 
+            height: 1080, 
+        },
+        min: {
+            width: 480, 
+            height: 270,
+        }
+    },
+
+    pixelArt: false, 
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 0 },
+            debug: true,
+            debugShowVelocity: false
+        }
+    },
+    scene: [
+        Boot,
+        Preloader,
+        MainMenu,
+        MainGame,
+        GameOver,
+        LoadingScene 
+    ]
+};
+
+const StartGame = (parent: string) => {
+
+    return new Game({ ...config, parent });
+
+}
+
+export default StartGame;
+
