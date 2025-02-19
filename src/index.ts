530fbb5185a2af3208b7af485422124f14e13930
admin <trcuong666@gmail.com>
2025-02-19 16:18:15 +0700

    add data file index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,44 @@
-import BaseController from "./game/controllers/BaseController";
-import BaseDTO from "./game/dtos/BaseDTO";
-import BaseService from "./game/services/BaseService";
-import BaseView from "./game/views/BaseView";
+import { Grid } from "matter";
+import BaseController from "./game/bases/BaseController";
+import BaseDTO from "./game/bases/BaseDTO";
+import BaseService from "./game/bases/BaseService";
+import BaseView from "./game/bases/BaseView";
+import { BlankTypeEnum } from "./game/math/components/enums/BlankTypeEnum";
+import { DrawDirectionEnum } from "./game/math/components/enums/DrawDirectionEnum";
+import { ElementTypeEnum } from "./game/math/components/enums/ElementTypeEnum";
+import { EquationSideEnum } from "./game/math/components/enums/EquationSideEnum";
+import { ExpressionDisplayTypeEnum } from "./game/math/components/enums/ExpressionDisplayTypeEnum";
+import { OperatorTypeEnum } from "./game/math/components/enums/OperatorTypeEnum";
+import { OptionLayoutEnum } from "./game/math/components/enums/OptionLayoutEnum";
+import { ICalculatable } from "./game/math/components/interfaces/ICalculatable";
+import { ICheckable } from "./game/math/components/interfaces/ICheckable";
+import { IComparable } from "./game/math/components/interfaces/IComparable";
+import { INumberable } from "./game/math/components/interfaces/INumberable";
+import { Cell } from "./game/math/systems/Cell";
+import { ArrayUtils } from "./game/math/utils/ArrrayUtils";
+import { DepthUtil } from "./game/math/utils/DepthUtil";
 import LoadingScene from "./game/scenes/LoadingScene";
+import { GridSystem } from "./game/math/systems/GridSystem";
+import { ExpressionHelper } from "./game/math/helpers/ExpressionHelper";
+import { TextStyleHelper } from "./game/math/helpers/TextStyleHelper";
+import { VSCH } from "./game/math/helpers/ViewSizeCalculatorHellper";
+import { NumberHelper } from "./game/math/helpers/NumberHelper";
+
 
 
 
 export { BaseController, BaseDTO, BaseService, LoadingScene, BaseView };
+// component-enums
+export { BlankTypeEnum, DrawDirectionEnum, ElementTypeEnum, EquationSideEnum, ExpressionDisplayTypeEnum, OperatorTypeEnum, OptionLayoutEnum }
+// component - interfaces
+export type { ICalculatable, ICheckable, IComparable, INumberable }
+// component - models
+
+// factories
+
+// helpers
+export { ExpressionHelper, NumberHelper, TextStyleHelper, VSCH }
+// systems
+export { Cell, Grid, GridSystem}
+// utils
+export { ArrayUtils, DepthUtil}
